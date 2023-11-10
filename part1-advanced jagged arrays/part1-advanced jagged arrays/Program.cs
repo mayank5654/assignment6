115efbb926fb97d77b2caf32989b1c98d687d0e8
mayank5654 <mayankkaushal99@gmail.com>
2023-11-10 16:11:49 +0530

    intial commit

---
+++ part1-advanced jagged arrays/part1-advanced jagged arrays/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1: Declare and Initialize Jagged Array of Integers
+            int[][] matrix = new int[3][];
+            matrix[0] = new int[] { 4, 7, 6, 3 };
+            matrix[1] = new int[] { 6, 6 };
+            matrix[2] = new int[] { 7, 8, 9 };
+
+            // Task 2.1: Access and store the value of the second element in the first row
+            int element = matrix[0][1];
+            Console.WriteLine($"Value of the second element in the first row: {element}");
+            // Task 2.2: Display the entire matrix using nested loop
+            Console.WriteLine("Matrix:");
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write(matrix[i][j] + " ");
+                }
+                Console.WriteLine(); // Move to the next line after each row
+            }
+
+            // Task 3.1: Modify the third element in the second row
+            if (matrix.Length > 1 && matrix[1].Length > 2)
+            {
+                matrix[1][2] = 15;
+                Console.WriteLine("Modification successful.");
+            }
+            else
+            {
+                Console.WriteLine("Cannot modify the third element in the second row. Insufficient elements.");
+            }
+
+            // Task 4.1: Declare and Initialize Jagged Array of Objects
+            object[][] objectMatrix = new object[2][];
+            objectMatrix[0] = new object[] { 10, "Hello", 3.14 };
+            objectMatrix[1] = new object[] { true, 'A', DateTime.Now };
+
+            // Task 4.2: Display the contents of the objectMatrix
+            Console.WriteLine("\nObject Matrix:");
+            for (int i = 0; i < objectMatrix.Length; i++)
+            {
+                for (int j = 0; j < objectMatrix[i].Length; j++)
+                {
+                    Console.WriteLine($"Type: {objectMatrix[i][j].GetType().Name}, Value: {objectMatrix[i][j]}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ part2-array list basics/part2-array list basics/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part2_array_list_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Task 1: ArrayList Initialization and Population
+                ArrayList studentGrades = new ArrayList();
+                studentGrades.AddRange(new int[] { 85, 92, 78, 95, 88 });
+
+                // Display initial student grades
+                Console.WriteLine("Task 1: Initial Student Grades:");
+                DisplayArrayList(studentGrades);
+                Console.WriteLine();
+
+                // Task 2: Display and Sum
+                Console.WriteLine("Task 2: Display and Sum");
+                DisplayArrayList(studentGrades);
+                int sum = CalculateSum(studentGrades);
+                Console.WriteLine($"Sum of Grades: {sum}\n");
+
+                // Task 3: Grade Addition
+                Console.WriteLine("Task 3: Grade Addition");
+                studentGrades.Add(90);
+                DisplayArrayList(studentGrades);
+                Console.WriteLine();
+
+                // Task 4: Grade Removal
+                Console.WriteLine("Task 4: Grade Removal");
+                studentGrades.Remove(78);
+                DisplayArrayList(studentGrades);
+                Console.WriteLine();
+
+                // Task 5: Grade Update
+                Console.WriteLine("Task 5: Grade Update");
+                int index95 = studentGrades.IndexOf(95);
+                if (index95 != -1)
+                {
+                    studentGrades[index95] = 96;
+                    DisplayArrayList(studentGrades);
+                }
+                else
+                {
+                    Console.WriteLine("Grade 95 not found in the list.");
+                }
+            }
+
+            // Helper method to display the contents of ArrayList
+            static void DisplayArrayList(ArrayList list)
+            {
+                foreach (var grade in list)
+                {
+                    Console.WriteLine(grade);
+                }
+            }
+
+            // Helper method to calculate the sum of ArrayList elements
+            static int CalculateSum(ArrayList list)
+            {
+                int sum = 0;
+                foreach (var grade in list)
+                {
+                    sum += (int)grade;
+                }
+                return sum;
+            }
+        }
+
+    }
+
+
+
+
+
+
+
